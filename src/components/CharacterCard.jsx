224a095fe32b4ab24c4f94afdda321e3c7a5de35
VictorRF <victor_rofh@hotmail.com>
2025-03-20 22:09:29 -0600

    Fix: General styles

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const CharacterCard = ({character, img, alt}) => {
   return (
-      <img className='episode-image' src={`/images/characters/${character}/${img}`} alt={alt} loading="lazy" />
+      <img className='image-bg' src={`/images/characters/${character}/${img}`} alt={alt} loading="lazy" />
   )
 }
