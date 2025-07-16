cb7a11d3f04fb85997fb174adb5c1888c9839c06
Victor Rodriguez <victorrofh@gmail.com>
2025-07-16 11:01:02 -0600

    Update: Character format images

--- src/data/personajes.js
+++ src/data/personajes.js
@@ -3,72 +3,72 @@ export const personajes = [
         "id": "vi",
         "nombre": "Vi",
         "descripcion": "Vi es una luchadora intrépida y excriminal reformada de Zaun, conocida por su actitud decidida y sus guanteletes Hextech.",
-        "imagen": "vi.jpg"
+        "imagen": "vi.webp"
       },
       {
         "id": "jinx",
         "nombre": "Jinx",
         "descripcion": "La hermana menor de Vi, convertida en una rebelde anárquica. Jinx tiene una inclinación por las explosiones y el caos.",
-        "imagen": "jinx.jpg"
+        "imagen": "jinx.webp"
       },
       {
         "id": "jayce",
         "nombre": "Jayce",
         "descripcion": "Un inventor idealista de Piltover que desarrolla la tecnología Hextech para mejorar la vida en la ciudad.",
-        "imagen": "jayce.jpg"
+        "imagen": "jayce.webp"
       },
       {
         "id": "viktor",
         "nombre": "Viktor",
         "descripcion": "Un científico brillante y colega de Jayce, quien lucha por superar sus limitaciones físicas y buscar el progreso.",
-        "imagen": "viktor.jpg"
+        "imagen": "viktor.webp"
       },
       {
         "id": "caitlyn",
         "nombre": "Caitlyn",
         "descripcion": "Sheriff de Piltover con una mentalidad astuta y una puntería impecable, trabaja para proteger la ciudad.",
-        "imagen": "Caitlyn.jpg"
+        "imagen": "Caitlyn.webp"
       },
       {
         "id": "silco",
         "nombre": "Silco",
         "descripcion": "El despiadado líder criminal de Zaun y figura paternal para Jinx, que busca la independencia de su ciudad.",
-        "imagen": "silco.jpg"
+        "imagen": "silco.webp"
       },
       {
         "id": "heimerdinger",
         "nombre": "Heimerdinger",
         "descripcion": "Un yordle sabio y respetado, miembro del Consejo de Piltover, dedicado a preservar el equilibrio en la ciudad.",
-        "imagen": "Heimerdinger.jpg"
+        "imagen": "Heimerdinger.webp"
       },
       {
         "id": "ekko",
         "nombre": "Ekko",
         "descripcion": "Un joven prodigio de Zaun que lidera a los Firelights y utiliza su ingenio para enfrentarse al caos.",
-        "imagen": "ekko.jpg"
+        "imagen": "ekko.webp"
       },
       {
         "id": "mel",
         "nombre": "Mel Medarda",
         "descripcion": "Una ambiciosa consejera de Piltover que impulsa las decisiones políticas con su inteligencia y manipulación.",
-        "imagen": "mel.jpg"
+        "imagen": "mel.webp"
       },
       {
         "id": "ambessa",
         "nombre": "Ambessa",
         "descripcion": "Ambessa, guerrera noxiana y madre de Mel, ha jurago proteger el nombre de su familia... incluso si eso la pone en contra de los deseos de su propia hija.",
-        "imagen": "ambessa.jpg"
+        "imagen": "ambessa.webp"
       },
       {
         "id": "isha",
         "nombre": "isha",
         "descripcion": "Isha, ",
-        "imagen": "isha.jpg"
+        "imagen": "isha.webp"
       },
       {
         "id": "warwick",
         "nombre": "Warwick",
         "descripcion": "Warwick, una bestia creada por Singed, se debate entre su ira primigenia y la humanidad fragmentada que aún habita en su interior.",
-        "imagen": "warwick.jpg"
+        "imagen": "warwick.webp"
       },
     ]
